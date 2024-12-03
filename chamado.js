91eba6b750c5838efb3979f3ff1dab927424ef6b
Juan Bressanim <juanlbressanim@gmail.com>
2024-12-02 22:27:08 -0300

    projeto (em andamento)

--- chamado.js
+++ chamado.js
@@ -3,32 +3,47 @@ var listaChamados=[];
 var i=0;
 
 function adicionarChamado(){
-    var chamado={};
+    var chamado=[];
     listaChamados[i]=chamado;
-    chamado.cliente=prompt(" Digite o nome do cliente: ");
-    chamado.data=prompt(" Digite a data do chamado: ");
+    listaChamados[i]=encerrado;    
+    chamado.nome=prompt(" Digite o nome do cliente: ");
+    chamado.dataChamado=prompt(" Digite a data do chamado: ");
+    chamado.dataResolucao=prompt(" Digite a data em que foi resolvido: ");
     chamado.local=prompt(" Digite o local do chamado: ");
     chamado.descricao=prompt(" Espefique a causa do chamado: ");
-
+    chamado.resolucao=prompt(" Digite a forma em que foi resolvido o chamado:");
+    i++;
 };
 
 function listarChamados(){
     for(var i=0; i<listaChamados.length;i++){
-    console.log(" Nome do cliente: ", listaChamados[i].cliente);
-    console.log(" Data: ", listaChamados[i].data);
-    console.log(" Local: ", listaChamados[i].local);
-    console.log(" Descrição: ", listaChamados[i].descricao);
-  }
+      console.log("\n      CHAMADO:        \n")
+      console.log(" Nome do cliente: ", listaChamados[i].nome);
+      console.log(" Data: ", listaChamados[i].dataChamado);
+      console.log(" Local: ", listaChamados[i].local);
+      console.log(" Descrição: ", listaChamados[i].descricao);
+
+  };
 };
 
-var continua = 's';
-while(continua == 's'){
-		var opcao = parseInt(prompt("O que deseja Fazer:\n 1- Adicionar chamado\n 2 - Listar Chamados"));
-		
-		if(opcao == 1){adicionarChamado();}else {listarChamados();}
-		
-		continua = prompt("Deseja fazer novas tarefas? Digite s para confirmar:");
+function fechamento(){
+  for(var i=0; i<listaChamados.length;i++){
+    console.log("\n      RESOLUÇÃO:        \n")
+    console.log(" Data da resolção: ", listaChamados[i].dataResolucao);
+    console.log(" Resolução final: ", listaChamados[i].resolucao);
     
+ };
 };
- 
-console.log(listaChamados[i]);
+
+i=0;
+
+var continua='s';
+while(continua=='s'){
+		var opcao=parseInt(prompt("O que deseja Fazer:\n 1- Adicionar chamado\n 2 - Listar Chamados:\n 3 - Resposta: "));
+		
+		if(opcao==1){adicionarChamado();}else{listarChamados();}
+    if(opcao==3){fechamento();} 
+		
+		continua=prompt("Deseja fazer novas tarefas? Digite s para confirmar:");
+};
+
